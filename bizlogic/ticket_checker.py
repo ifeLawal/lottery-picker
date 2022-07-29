d4802b669d85a71cb194d8bb9c070a1552ccd8e8
Ife <il693@nyu.edu>
2022-07-29 13:02:54 -0400

    fixed code so there are no errors

--- bizlogic/ticket_checker.py
+++ bizlogic/ticket_checker.py
@@ -1,14 +1,15 @@
 from datetime import date
+from bizlogic import constants
 from bizlogic.mega_millions import Session
 from bizlogic.number_pickers import create_tickets, random_ticket_creation, ordered_megaball
 from datastore.models.mega_millions import Winners
 
-def check_tickets(ticket: list, date: date) -> str:
+def check_tickets(ticket: list, date: date) -> list:
     draw_date = "07/26/22"
     with Session() as session:
         winner = session.query(Winners).filter(Winners.draw_date == draw_date)
-        winning_ticket = {str(winner.first_number):""} [, winner.second_number, winner.third_number, winner.fourth_number, winner.fifth_number, winner.mega_ball]
+        # winning_ticket = {str(winner.first_number):""} [, winner.second_number, winner.third_number, winner.fourth_number, winner.fifth_number, winner.mega_ball]
     
     # pull the latest winning numbers
     # check if your tickets won
-    pass
+    return [{"23": constants.MATCHED, "43": constants.UNMATCHED}]
