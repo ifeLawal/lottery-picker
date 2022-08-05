3b388cf7887b8078478fc8c37dd104f4872a2c9a
Ife <il693@nyu.edu>
2022-08-04 23:35:30 -0400

    wrote the logic for checking winnings. TODO writ the test and test it

--- bizlogic/ticket_checker.py
+++ bizlogic/ticket_checker.py
@@ -3,6 +3,20 @@ from bizlogic import constants
 from bizlogic.mega_millions import Session
 from datastore.models.mega_millions import Winners
 
+winning_options = {
+    "5win": 1000000,
+    "5winmega": 1000000,
+    "4win": 500,
+    "4winmega": 10000,
+    "3win": 10,
+    "3winmega": 200,
+    "2win": 0,
+    "2winmega": 10,
+    "1win": 0,
+    "1winmega": 4,
+    "mega": 2
+}
+
 def check_list_of_tickets(ticket: list, date: date):
     draw_date = "07/26/22"
     with Session() as session:
@@ -30,26 +44,44 @@ def get_number_matches_on_ticket(ticket: list, regular_winners: object, mega_bal
                 mega_ball_number_results[str_num] = constants.MATCHED
             else:
                 mega_ball_number_results[str_num] = constants.UNMATCHED
-    return {constants.REGULAR_NUMBERS_PROPERTY_NAME: regular_number_results, constants.MEGA_BALL_NUMBER_PROPERTY_NAME: mega_ball_number_results}
+    return regular_number_results, mega_ball_number_results
+    # return {constants.REGULAR_NUMBERS_PROPERTY_NAME: regular_number_results, constants.MEGA_BALL_NUMBER_PROPERTY_NAME: mega_ball_number_results}
 
 """
 ticket_matching expected sample object
+tuple
 {"regular_numbers":{"23": "match found", "43": "no match", "56": "no match", "1": "no match", "10": "no match"}, "mega_ball_number": {"23": "match found"}}]
 """
-def check_winnings_for_a_ticket(ticket_matching: object) -> int:
+def check_winnings_for_a_ticket(ticket: object, regular_winners: object, mega_ball_winner: object) -> int:
     # If a player matches all five numbers, but not the “Mega Ball,” then they would win $1 million, according to officials.
     # Matching four numbers plus the “Mega Ball” would net the player $10,000. Matching four numbers without the “Mega Ball” would earn the player a $500 prize.
     # Matching three numbers and the “Mega Ball” is worth $200.
     # Matching three numbers, or matching two numbers and the “Mega Ball,” will win a player $10.
     # Matching one number and the “Mega Ball” is worth $4, and matching the “Mega Ball” is worth a $2 prize.
-    regular_number_winners = 0
-    mega_ball_match = 0
-    regular_numbers = ticket_matching[constants.REGULAR_NUMBERS_PROPERTY_NAME]
-    mega_ball_number = ticket_matching[constants.MEGA_BALL_NUMBER_PROPERTY_NAME]
-    for mega_ball in ticket_matching[constants.MEGA_BALL_NUMBER_PROPERTY_NAME]:
-        # if mega_ball[]
-        pass
-    for regular_numbers in ticket_matching[constants.REGULAR_NUMBERS_PROPERTY_NAME]:
-        pass    
-        
-    return 0
+    regular_number_winner_count = 0
+    mega_ball_winner_count = 0
+    (regular_number_winners, mega_ball_winner) = get_number_matches_on_ticket(ticket=ticket, regular_winners=regular_winners, mega_ball_winner=mega_ball_winner)
+    for number in mega_ball_winner:
+        if mega_ball_winner[number] == constants.MATCHED:
+            mega_ball_winner_count += 1
+            print("we got the mega ball")
+    for number in regular_number_winners:
+        if regular_number_winners[number] == constants.MATCHED:
+            regular_number_winner_count += 1
+    winnings = 0
+    append = ""
+    if mega_ball_winner_count >= 1:
+        append = "mega"
+        winnings = 2
+    if regular_number_winner_count == 5:
+        return winning_options["5win"+append]
+    if regular_number_winner_count == 4:
+        return winning_options["4win"+append]
+    if regular_number_winner_count == 3:
+        return winning_options["3win"+append]
+    if regular_number_winner_count == 2:
+        return winning_options["2win"+append]
+    if regular_number_winner_count == 1:
+        return winning_options["1win"+append]
+
+    return winnings
